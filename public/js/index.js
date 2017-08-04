7077bb05d8299c1bf70c124fe2c9fe7cb3f175ab
jorgesantosn <netneill@hotmail.com>
2017-08-03 19:39:55 -0600

    functions was added

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
-setTimeout(function(){   
-	$("#idQuiz").text(Math.round(Math.random()*100))  
-},1000);
+var datos = {};
 
 function getQuiz(){
 	var numeroAleatorio = Math.round(Math.random()*100);
@@ -8,6 +6,33 @@ function getQuiz(){
 	$("#txtNombre").val(numeroAleatorio);
 }
 
-$("#getId").click(function(){ 
-	getQuiz();
-});
+function insertarParrafo(){
+	var nuevoParrafo = $("#txtParrafo").val();
+	var titulo = $("#txtTitulo").val();
+	datos.parrafos.push({"titulo": titulo, "parrafo":parrafo});
+}
+
+function mostrarParrafos(){
+	datos.parrafos.forEach(function(index,val){
+		console.log("index",val);
+	})
+}
+
+function main(){
+	//DATOS INICIALES
+	datos.parrafos = {};
+
+	// EVENTOS
+	
+	$("#btnGuardar").click(function(){ 
+		insertarParrafo();
+	});
+
+	//LOGICA INICIAL.
+	setTimeout(function(){   
+		$("#idQuiz").text(Math.round(Math.random()*100))  
+	},1000);
+
+};
+
+main();
