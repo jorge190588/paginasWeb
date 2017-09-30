5739a0d8f2fd9cc26efcfd6130e95274808ee414
jorgesantosn <netneill@hotmail.com>
2017-09-30 14:32:52 -0600

    message function was created

--- sockets/server.js
+++ sockets/server.js
@@ -13,6 +13,11 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+
+  socket.on('chat message', function(msg){
+   	console.log('message: ' + msg);
+  });
+
 });
 
 http.listen(3000, function(){
